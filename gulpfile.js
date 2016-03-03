7da8c7807630bb35b9e6ea960c2f515c7a298aa7
Inderpreet Singh <inder@wastedcode.com>
2016-03-03 15:46:24 -0800

    Add a gulp task for css

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,38 @@
 var gulp = require('gulp');
+
 var gls = require('gulp-live-server');
+
+// This is used for CSS
+var concat = require('gulp-continuous-concat');
+var minifyCss = require('gulp-minify-css');
+var sass = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
+var watch = require('gulp-watch');
+
+// We want to run certain tasks only when in production
+var environments = require('gulp-environments');
+var dev = environments.development;
+var prod = environments.production;
 
 gulp.task('default', function() {
 });
 
-gulp.task('serve', function() {
+gulp.task('compile-scss', function() {
+  gulp.src([
+      'app/stylesheets/**/*.scss',
+      '!app/stylesheets/**/_*.scss'
+    ])
+    .pipe(watch('app/stylesheets/**/*.scss'))
+    .pipe(sourcemaps.init())
+    .pipe(sass({ indentedSyntax: false, errLogToConsole: true }))
+    .pipe(concat('application.css'))
+    .pipe(prod(minifyCss()))
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('static/stylesheets'))
+});
+
+
+gulp.task('serve', ['compile-scss'], function() {
   //1. run your script as a server
   var server = gls.new('app.js');
   server.start();
