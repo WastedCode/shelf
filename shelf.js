7e7278f4e9b4e03a5d04d9e1e6fb9defc7bfe5cb
Inderpreet Singh <inder@wastedcode.com>
2016-03-03 18:28:35 -0800

    Log all requests

--- shelf.js
+++ shelf.js
@@ -2,6 +2,12 @@ var express = require('express');
 var app = express();
 var rootRoutes = require('./app/routes/root');
 
+var requestLogger = function (req, res, next) {
+  console.log(Date.now() + " - " + req.method + ": " + req.originalUrl);
+  next();
+};
+
+app.use(requestLogger);
 app.use(express.static('./app/static'));
 app.use('/', rootRoutes);
 
